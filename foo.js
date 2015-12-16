4266ca442f2662ec5dc5491d0c11a69c852d747f
Pete Corey <petecorey@gmail.com>
2015-12-16 06:49:28 -0800

    Meteor.call test

--- foo.js
+++ foo.js
@@ -2,10 +2,19 @@ export class Foo {
 
   constructor(Meteor) {
     this.Meteor = Meteor;
+    Meteor.methods({
+      "foo": function() {
+        return this.bar();
+      }
+    });
   }
 
   bar() {
     return "woo!";
+  }
+
+  doSomething() {
+    this.Meteor.call("something", this.bar());
   }
 
 }
--- tests/foo.js
+++ tests/foo.js
@@ -1,13 +1,30 @@
-const expect = require("chai").expect;
+import * as chai from "chai";
+import * as sinon from "sinon";
+const expect = chai.expect;
+chai.use(require("sinon-chai"));
+
 import {Foo} from "../foo";
 
-const Meteor = {};
-
 describe("foo", function() {
+
+  let Meteor;
+  beforeEach(function() {
+    Meteor = {
+      methods: sinon.spy(),
+      call: sinon.spy()
+    };
+  });
+
   it("returns woo", function() {
     let foo = new Foo(Meteor);
     let bar = foo.bar();
-
     expect(bar).to.equal("woo!");
   });
+
+  it("does something", function() {
+    let foo = new Foo(Meteor);
+    foo.doSomething();
+    expect(Meteor.call).to.have.been.calledWith("something", "woo!");
+  });
+
 });
